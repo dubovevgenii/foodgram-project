dcf11b1bc62499f226835f4a4dfe1023cb0c6c2f
Evgenii <dubovevgenii85@gmail.com>
2021-02-08 21:57:08 +0300

    Initial product commit

---
+++ recipes/static/js/api/Api.js
@@ -0,0 +1,133 @@
+
+class Api {
+    constructor(apiUrl) {
+        this.apiUrl =  apiUrl;
+    }
+  getPurchases () {
+    return fetch(`api/v1/purchases/`, {
+      headers: {
+        'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then( e => {
+          if(e.ok) {
+              return e.json()
+          }
+          return Promise.reject(e.statusText)
+      })
+  }
+  addPurchases (id) {
+    return fetch(`api/v1/purchases/`, {
+      method: 'POST',
+      headers: {
+        'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: id
+      })
+    })
+      .then( e => {
+          if(e.ok) {
+              return e.json()
+          }
+          return Promise.reject(e.statusText)
+      })
+  }
+  removePurchases (id){
+    return fetch(`api/v1/purchases/${id}/`, {
+      method: 'DELETE',
+      headers: {
+        'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then( e => {
+          if(e.ok) {
+              return e.json()
+          }
+          return Promise.reject(e.statusText)
+      })
+  }
+  addSubscriptions (id) {
+    return fetch(`api/v1/subscribes/`, {
+      method: 'POST',
+      headers: {
+        'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: id
+      })
+    })
+      .then( e => {
+          if(e.ok) {
+              return e.json()
+          }
+          return Promise.reject(e.statusText)
+      })
+  }
+  removeSubscriptions (id) {
+    return fetch(`api/v1/subscribes/${id}/`, {
+      method: 'DELETE',
+      headers: {
+        'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then( e => {
+          if(e.ok) {
+              return e.json()
+          }
+          return Promise.reject(e.statusText)
+      })
+  }
+  addFavorites (id)  {
+    return fetch(`api/v1/favorites/`, {
+      method: 'POST',
+      headers: {
+        'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: id
+      })
+    })
+        .then( e => {
+            if(e.ok) {
+                return e.json()
+            }
+            return Promise.reject(e.statusText)
+        })
+  }
+  removeFavorites (id) {
+    return fetch(`api/v1/favorites/${id}/`, {
+      method: 'DELETE',
+      headers: {
+        'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value,
+        'Content-Type': 'application/json'
+      }
+    })
+        .then( e => {
+            if(e.ok) {
+                return e.json()
+            }
+            return Promise.reject(e.statusText)
+        })
+  }
+    getIngredients  (text)  {
+        return fetch(`api/v1/ingredients?query=${text}`, {
+            headers: {
+                'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value,
+                'Content-Type': 'application/json'
+            }
+        })
+            .then( e => {
+                if(e.ok) {
+                    return e.json()
+                }
+                return Promise.reject(e.statusText)
+            })
+    }
+}
