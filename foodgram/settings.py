ad5f794d11bafc9d41fd14647c8f9485010676c1
Evgenii <dubovevgenii85@gmail.com>
2021-02-14 22:45:36 +0300

    Media replaced

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 SITE_ID = 1
 
